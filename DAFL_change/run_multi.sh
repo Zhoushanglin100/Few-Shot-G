10998a5e57016b9637aad284f2e5302c1ce30648
Zhoushanglin100 <shanglin.zhou@uconn.edu>
2021-11-27 21:27:42 -0500

    1127 sh

--- DAFL_change/run_multi.sh
+++ DAFL_change/run_multi.sh
@@ -1,3 +1,6 @@
-python3 DAFLDeepinvert-train_v6_tmp_v2.py --fix_G --train_G --n_epochs 2000 --lr_G 0.001 --latent_dim 1000 --ext 10G1000
+# python3 DAFLDeepinvert-train_v6_tmp_v2.py --fix_G --train_G --n_epochs 2000 --lr_G 0.001 --latent_dim 1000 --ext 10G1000
+# python3 DAFLDeepinvert-train_v6_tmp_v2.py --fix_G --train_S --n_epochs 2000 --latent_dim 1000 --ext 10G1000
 
-python3 DAFLDeepinvert-train_v6_tmp_v2.py --fix_G --train_S --n_epochs 2000 --latent_dim 1000 --ext 10G1000
+
+# python3 DAFLDeepinvert-train_v6_tmp_v2.py --fix_G --train_G --n_epochs 2000 --lr_G 0.001 --latent_dim 1000 --ext 10G1000
+python3 DAFLDeepinvert-train_v6_tmp_v2.py --fix_G --train_S --n_epochs 2000 --latent_dim 3000 --ext 10G3000NEW
