559914b91e1d4ae1f1f65c1ecf6ee7f53edb6ab5
Zhoushanglin100 <shanglin.zhou@uconn.edu>
2022-02-05 04:33:37 -0500

    0205

--- DAFL_change/run_v7_v3.sh
+++ DAFL_change/run_v7_v3.sh
@@ -1,32 +1,29 @@
 # ### finish
 # bash run_cifar100_v4.sh output 8 64 100 3000 $1 s3 &
-# bash run_cifar100_v4.sh output 8 64 100 1000 $1 &
-
-# bash run_cifar100_v4.sh input 8 64 10 1000 $1 &
-# bash run_cifar100_v4.sh input 8 64 10 3000 $1 &
+# bash run_cifar100_v4.sh output 8 64 100 1000 $1 s3 &
+# bash run_cifar100_v4.sh output 8 64 10 3000 $1 s3 &
 # bash run_cifar100_v4.sh output 8 64 10 1000 $1 s3 &
+
+# bash run_cifar100_v4.sh input 8 64 10 1000 $1 s3 &
+# bash run_cifar100_v4.sh input 8 64 10 3000 $1 s3 &
 # bash run_cifar100_v4.sh input 8 64 100 1000 $1 s3 &
+# bash run_cifar100_v4.sh input 8 64 100 3000 $1 s3 &
 # bash run_cifar100_v4.sh input 32 64 100 1000 $1 s3 &
 
-# ### runing
-bash run_cifar100_v4.sh output 32 64 100 1000 $1 s3 &
-bash run_cifar100_v4.sh output 32 64 10 1000 $1 s3 &
-bash run_cifar100_v4.sh output 32 64 100 3000 $1 s3 &
-
-
 # ### crashed
-bash run_cifar100_v4.sh output 8 64 10 3000 $1 s3 &
+bash run_cifar100_v4.sh output 32 64 100 1000 $1 s3 &
+bash run_cifar100_v4.sh output 32 64 100 3000 $1 s3 &
+bash run_cifar100_v4.sh output 32 64 10 1000 $1 s3 &
 bash run_cifar100_v4.sh output 32 64 10 3000 $1 s3 &
 
 # #
-bash run_cifar100_v4.sh input 8 64 100 3000 $1 s3 &
 bash run_cifar100_v4.sh input 32 64 10 1000 $1 s3 &
 bash run_cifar100_v4.sh input 32 64 100 3000 $1 s3 &
 bash run_cifar100_v4.sh input 32 64 10 3000 $1 s3 &
 
-# #
-bash run_cifar100_v5.sh output 16 128 10 3000 $1 128 s2 &
-bash run_cifar100_v5.sh output 16 128 100 3000 $1 128 s2 &
+# # #
+# bash run_cifar100_v5.sh output 16 128 10 3000 $1 128 s3 &
+# bash run_cifar100_v5.sh output 16 128 100 3000 $1 128 s3 &
 
-bash run_cifar100_v5.sh input 16 128 10 3000 $1 128 s2 &
-bash run_cifar100_v5.sh input 16 128 100 3000 $1 128 s2 &
+# bash run_cifar100_v5.sh input 16 128 10 3000 $1 128 s3 &
+# bash run_cifar100_v5.sh input 16 128 100 3000 $1 128 s3 &
