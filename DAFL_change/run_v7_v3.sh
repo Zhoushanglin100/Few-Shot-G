7ea85e75c5b75207fed15c2960ea7d67e2760533
Zhoushanglin100 <shanglin.zhou@uconn.edu>
2022-02-06 05:25:26 -0500

    0206

--- DAFL_change/run_v7_v3.sh
+++ DAFL_change/run_v7_v3.sh
@@ -10,20 +10,23 @@
 # bash run_cifar100_v4.sh input 8 64 100 3000 $1 s3 &
 # bash run_cifar100_v4.sh input 32 64 100 1000 $1 s3 &
 
+# bash run_cifar100_v4.sh output 32 64 10 3000 $1 s3 &
+
+# ### runing
+# bash run_cifar100_v4.sh output 32 64 100 1000 $1 s3 &
+
 # ### crashed
-bash run_cifar100_v4.sh output 32 64 100 1000 $1 s3 &
 bash run_cifar100_v4.sh output 32 64 100 3000 $1 s3 &
 bash run_cifar100_v4.sh output 32 64 10 1000 $1 s3 &
-bash run_cifar100_v4.sh output 32 64 10 3000 $1 s3 &
 
 # #
 bash run_cifar100_v4.sh input 32 64 10 1000 $1 s3 &
 bash run_cifar100_v4.sh input 32 64 100 3000 $1 s3 &
 bash run_cifar100_v4.sh input 32 64 10 3000 $1 s3 &
 
-# # #
-# bash run_cifar100_v5.sh output 16 128 10 3000 $1 128 s3 &
-# bash run_cifar100_v5.sh output 16 128 100 3000 $1 128 s3 &
+# #
+bash run_cifar100_v5.sh output 16 128 10 3000 $1 128 s3 &
+bash run_cifar100_v5.sh output 16 128 100 3000 $1 128 s3 &
 
-# bash run_cifar100_v5.sh input 16 128 10 3000 $1 128 s3 &
-# bash run_cifar100_v5.sh input 16 128 100 3000 $1 128 s3 &
+bash run_cifar100_v5.sh input 16 128 10 3000 $1 128 s3 &
+bash run_cifar100_v5.sh input 16 128 100 3000 $1 128 s3 &
