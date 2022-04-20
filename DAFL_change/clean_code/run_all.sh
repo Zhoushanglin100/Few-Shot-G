25125911def73c44cbf1796ce7883be3a27f86d8
Zhoushanglin100 <zhoushanglin100@gmail.com>
2022-04-20 18:19:31 -0400

    0420 run S

--- DAFL_change/clean_code/run_all.sh
+++ DAFL_change/clean_code/run_all.sh
@@ -18,39 +18,49 @@ export imagenet_path=$2
 # done
 
 # -----------------
-for G_bz in 128 256
-do
-    for latent_dim in 1000 3000 5000
-    do
-        bash run_hpc.sh $partion s2 50 resnet34 resnet $G_bz 128 10 $latent_dim 0.1 0.01 0.1 $imagenet_path &
-        bash run_hpc.sh $partion s2 100 resnet34 resnet $G_bz 128 10 $latent_dim 0.1 0.01 0.1 $imagenet_path &
-        bash run_hpc.sh $partion s2 50 vgg16 vgg $G_bz 128 10 $latent_dim 0.1 0.01 0.1 $imagenet_path &
-        bash run_hpc.sh $partion s2 100 vgg16 vgg $G_bz 128 10 $latent_dim 0.1 0.01 0.1 $imagenet_path &
-    done
-done
+# for G_bz in 128 256
+# do
+#     for latent_dim in 1000 3000 5000
+#     do
+#         bash run_hpc.sh $partion s2 50 resnet34 resnet $G_bz 128 10 $latent_dim 0.1 0.01 0.1 $imagenet_path &
+#         bash run_hpc.sh $partion s2 100 resnet34 resnet $G_bz 128 10 $latent_dim 0.1 0.01 0.1 $imagenet_path &
+#         bash run_hpc.sh $partion s2 50 vgg16 vgg $G_bz 128 10 $latent_dim 0.1 0.01 0.1 $imagenet_path &
+#         bash run_hpc.sh $partion s2 100 vgg16 vgg $G_bz 128 10 $latent_dim 0.1 0.01 0.1 $imagenet_path &
+#     done
+# done
 
-for G_bz in 128 256
-do
-    for latent_dim in 1000
-    do
-        bash run_hpc.sh $partion s2 10 resnet34 resnet $G_bz 128 10 $latent_dim 0.1 0.01 0.1 $imagenet_path &
-        bash run_hpc.sh $partion s2 10 vgg16 vgg $G_bz 128 10 $latent_dim 0.1 0.01 0.1 $imagenet_path &
-    done
-done
+# for G_bz in 128 256
+# do
+#     for latent_dim in 1000
+#     do
+#         bash run_hpc.sh $partion s2 10 resnet34 resnet $G_bz 128 10 $latent_dim 0.1 0.01 0.1 $imagenet_path &
+#         bash run_hpc.sh $partion s2 10 vgg16 vgg $G_bz 128 10 $latent_dim 0.1 0.01 0.1 $imagenet_path &
+#     done
+# done
 
 # bash run_hpc.sh $partion s2 10 resnet34 resnet $G_bz 128 10 $latent_dim 0.1 0.01 0.1 $imagenet_path &
 # bash run_hpc.sh $partion s2 10 vgg16 vgg $G_bz 128 10 $latent_dim 0.1 0.01 0.1 $imagenet_path &
 
 # -----------------
 
-# bash run_hpc.sh $partion s3 10 resnet34 resnet $G_bz $S_bz 10 $latent_dim 0.1 0.01 $ratio $imagenet_path
-# bash run_hpc.sh $partion s3 50 resnet34 resnet $G_bz $S_bz 10 $latent_dim 0.1 0.01 $ratio $imagenet_path
-# bash run_hpc.sh $partion s3 100 resnet34 resnet $G_bz $S_bz 10 $latent_dim 0.1 0.01 $ratio $imagenet_path
+for G_bz in 128 256; do
+for S_bz in 64 128 256; do
+for latent_dim in 100 1000 3000 5000; do
+for ratio in $(seq 0 0.1 1); do
 
-# bash run_hpc.sh $partion s3 10 vgg16 vgg $G_bz $S_bz 10 $latent_dim 0.1 0.01 $ratio $imagenet_path
-# bash run_hpc.sh $partion s3 50 vgg16 vgg $G_bz $S_bz 10 $latent_dim 0.1 0.01 $ratio $imagenet_path
-# bash run_hpc.sh $partion s3 100 vgg16 vgg $G_bz $S_bz 10 $latent_dim 0.1 0.01 $ratio $imagenet_path
+bash run_hpc.sh $partion s3 10 resnet34 resnet $G_bz $S_bz 10 $latent_dim 0.1 0.01 $ratio $imagenet_path &
+bash run_hpc.sh $partion s3 50 resnet34 resnet $G_bz $S_bz 10 $latent_dim 0.1 0.01 $ratio $imagenet_path &
+bash run_hpc.sh $partion s3 100 resnet34 resnet $G_bz $S_bz 10 $latent_dim 0.1 0.01 $ratio $imagenet_path &
 
-# bash run_hpc.sh $partion s3 10 vgg16 resnet $G_bz $S_bz 10 $latent_dim 0.1 0.01 $ratio $imagenet_path
-# bash run_hpc.sh $partion s3 50 vgg16 resnet $G_bz $S_bz 10 $latent_dim 0.1 0.01 $ratio $imagenet_path
-# bash run_hpc.sh $partion s3 100 vgg16 resnet $G_bz $S_bz 10 $latent_dim 0.1 0.01 $ratio $imagenet_path
+bash run_hpc.sh $partion s3 10 vgg16 vgg $G_bz $S_bz 10 $latent_dim 0.1 0.01 $ratio $imagenet_path &
+bash run_hpc.sh $partion s3 50 vgg16 vgg $G_bz $S_bz 10 $latent_dim 0.1 0.01 $ratio $imagenet_path &
+bash run_hpc.sh $partion s3 100 vgg16 vgg $G_bz $S_bz 10 $latent_dim 0.1 0.01 $ratio $imagenet_path &
+
+bash run_hpc.sh $partion s3 10 vgg16 resnet $G_bz $S_bz 10 $latent_dim 0.1 0.01 $ratio $imagenet_path &
+bash run_hpc.sh $partion s3 50 vgg16 resnet $G_bz $S_bz 10 $latent_dim 0.1 0.01 $ratio $imagenet_path &
+bash run_hpc.sh $partion s3 100 vgg16 resnet $G_bz $S_bz 10 $latent_dim 0.1 0.01 $ratio $imagenet_path &
+
+done
+done
+done
+done
