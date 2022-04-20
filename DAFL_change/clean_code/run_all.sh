f8a7363ed2ee4fba678020a52e0f33103fa019a8
Zhoushanglin100 <zhoushanglin100@gmail.com>
2022-04-20 02:48:43 -0400

    0420 remove sample10

--- DAFL_change/clean_code/run_all.sh
+++ DAFL_change/clean_code/run_all.sh
@@ -13,7 +13,7 @@ export imagenet_path=$2
 # do
 #     for teacher in resnet34 vgg16
 #     do
-#         bash run_local.sh $partion s1 $sample_bz $teacher resnet 128 128 10 1000 0.1 0.01 0.5 $imagenet_path &
+#         bash run_hpc.sh $partion s1 $sample_bz $teacher resnet 128 128 10 1000 0.1 0.01 0.5 $imagenet_path &
 #     done
 # done
 
@@ -22,25 +22,35 @@ for G_bz in 128 256
 do
     for latent_dim in 1000 3000 5000
     do
-    bash run_hpc.sh $partion s2 10 resnet34 resnet $G_bz 128 10 $latent_dim 0.1 0.01 0.1 $imagenet_path &
-    bash run_hpc.sh $partion s2 50 resnet34 resnet $G_bz 128 10 $latent_dim 0.1 0.01 0.1 $imagenet_path &
-    bash run_hpc.sh $partion s2 100 resnet34 resnet $G_bz 128 10 $latent_dim 0.1 0.01 0.1 $imagenet_path &
-    bash run_hpc.sh $partion s2 10 vgg16 vgg $G_bz 128 10 $latent_dim 0.1 0.01 0.1 $imagenet_path &
-    bash run_hpc.sh $partion s2 50 vgg16 vgg $G_bz 128 10 $latent_dim 0.1 0.01 0.1 $imagenet_path &
-    bash run_hpc.sh $partion s2 100 vgg16 vgg $G_bz 128 10 $latent_dim 0.1 0.01 0.1 $imagenet_path &
+        bash run_hpc.sh $partion s2 50 resnet34 resnet $G_bz 128 10 $latent_dim 0.1 0.01 0.1 $imagenet_path &
+        bash run_hpc.sh $partion s2 100 resnet34 resnet $G_bz 128 10 $latent_dim 0.1 0.01 0.1 $imagenet_path &
+        bash run_hpc.sh $partion s2 50 vgg16 vgg $G_bz 128 10 $latent_dim 0.1 0.01 0.1 $imagenet_path &
+        bash run_hpc.sh $partion s2 100 vgg16 vgg $G_bz 128 10 $latent_dim 0.1 0.01 0.1 $imagenet_path &
     done
 done
 
+for G_bz in 128 256
+do
+    for latent_dim in 1000
+    do
+        bash run_hpc.sh $partion s2 10 resnet34 resnet $G_bz 128 10 $latent_dim 0.1 0.01 0.1 $imagenet_path &
+        bash run_hpc.sh $partion s2 10 vgg16 vgg $G_bz 128 10 $latent_dim 0.1 0.01 0.1 $imagenet_path &
+    done
+done
+
+# bash run_hpc.sh $partion s2 10 resnet34 resnet $G_bz 128 10 $latent_dim 0.1 0.01 0.1 $imagenet_path &
+# bash run_hpc.sh $partion s2 10 vgg16 vgg $G_bz 128 10 $latent_dim 0.1 0.01 0.1 $imagenet_path &
+
 # -----------------
 
-# bash run_local.sh $partion s3 10 resnet34 resnet $G_bz $S_bz 10 $latent_dim 0.1 0.01 $ratio $imagenet_path
-# bash run_local.sh $partion s3 50 resnet34 resnet $G_bz $S_bz 10 $latent_dim 0.1 0.01 $ratio $imagenet_path
-# bash run_local.sh $partion s3 100 resnet34 resnet $G_bz $S_bz 10 $latent_dim 0.1 0.01 $ratio $imagenet_path
+# bash run_hpc.sh $partion s3 10 resnet34 resnet $G_bz $S_bz 10 $latent_dim 0.1 0.01 $ratio $imagenet_path
+# bash run_hpc.sh $partion s3 50 resnet34 resnet $G_bz $S_bz 10 $latent_dim 0.1 0.01 $ratio $imagenet_path
+# bash run_hpc.sh $partion s3 100 resnet34 resnet $G_bz $S_bz 10 $latent_dim 0.1 0.01 $ratio $imagenet_path
 
-# bash run_local.sh $partion s3 10 vgg16 vgg $G_bz $S_bz 10 $latent_dim 0.1 0.01 $ratio $imagenet_path
-# bash run_local.sh $partion s3 50 vgg16 vgg $G_bz $S_bz 10 $latent_dim 0.1 0.01 $ratio $imagenet_path
-# bash run_local.sh $partion s3 100 vgg16 vgg $G_bz $S_bz 10 $latent_dim 0.1 0.01 $ratio $imagenet_path
+# bash run_hpc.sh $partion s3 10 vgg16 vgg $G_bz $S_bz 10 $latent_dim 0.1 0.01 $ratio $imagenet_path
+# bash run_hpc.sh $partion s3 50 vgg16 vgg $G_bz $S_bz 10 $latent_dim 0.1 0.01 $ratio $imagenet_path
+# bash run_hpc.sh $partion s3 100 vgg16 vgg $G_bz $S_bz 10 $latent_dim 0.1 0.01 $ratio $imagenet_path
 
-# bash run_local.sh $partion s3 10 vgg16 resnet $G_bz $S_bz 10 $latent_dim 0.1 0.01 $ratio $imagenet_path
-# bash run_local.sh $partion s3 50 vgg16 resnet $G_bz $S_bz 10 $latent_dim 0.1 0.01 $ratio $imagenet_path
-# bash run_local.sh $partion s3 100 vgg16 resnet $G_bz $S_bz 10 $latent_dim 0.1 0.01 $ratio $imagenet_path
+# bash run_hpc.sh $partion s3 10 vgg16 resnet $G_bz $S_bz 10 $latent_dim 0.1 0.01 $ratio $imagenet_path
+# bash run_hpc.sh $partion s3 50 vgg16 resnet $G_bz $S_bz 10 $latent_dim 0.1 0.01 $ratio $imagenet_path
+# bash run_hpc.sh $partion s3 100 vgg16 resnet $G_bz $S_bz 10 $latent_dim 0.1 0.01 $ratio $imagenet_path
